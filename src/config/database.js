6afe6dc9894fa9d6cb2d38d9a74318ad65e626c4
Ricardo <ricardo.fontes@fatec.sp.gov.br>
2025-10-07 20:40:35 -0300

    atualiazcoes conexoes

--- src/config/database.js
+++ src/config/database.js
@@ -47,10 +47,10 @@ if (!isAzureConfigured) {
 }
 
 // Usar variáveis de ambiente com fallbacks para valores padrão
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USER = process.env.DB_USER || 'root';
+const DB_HOST = process.env.DB_HOST || '';
+const DB_USER = process.env.DB_USER || '';
 const DB_PASS = process.env.DB_PASS || '';
-const DB_NAME = process.env.DB_NAME || 'whitelabel';
+const DB_NAME = process.env.DB_NAME || '';
 const DB_PORT = process.env.DB_PORT || 3306;
 
 // Em ambiente de produção, use valores mais robustos
--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "locadora-mysql.mysql.database.azure.com",
-    user: process.env.DB_USER || "adminazure",
-    password: process.env.DB_PASS || "SuaSenha@123",
-    database: process.env.DB_NAME || "locadora",
+    host: process.env.DB_HOST || "server-bd-cn1.mysql.database.azure.com",
+    user: process.env.DB_USER || "useradmin",
+    password: process.env.DB_PASS || "admin@123",
+    database: process.env.DB_NAME || "rickveiculos",
     port: 3306,
     ssl: {
         rejectUnauthorized: true
